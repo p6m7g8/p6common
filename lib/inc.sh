8eefc293398e281d97f1e93bac287f07b56d7a17
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-08-08 23:24:01 -0400

    checkpoint

---
+++ lib/inc.sh
@@ -0,0 +1,12 @@
+include_file() {
+    local file="$1"
+
+    file_load "$file"
+}
+
+include_module() {
+    local module="$1"
+    local start="${2:-init.cfg}"
+
+    file_load "$module/$start"
+}

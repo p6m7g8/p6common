1e24ed005c941e379aaaf8ffdf0bb0bae8637666
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-07 20:49:04 -0500

    componentize debug

--- lib/debug.sh
+++ lib/debug.sh
@@ -11,7 +11,7 @@
 p6_debug__debug() {
     local msg="$1" # msg to log
 
-    p6_debug "debug: $msg"
+    p6_debug "p6_debug: $msg"
 
     p6_return_void
 }
@@ -46,9 +46,15 @@ p6_verbose() {
 #>
 ######################################################################
 p6_debug() {
+    local msg="$1"
+
+    local systems="$P6_DEBUG"
+    local system=$(p6_echo "$msg" | cut -d : -f 1)
 
     if p6_debugging; then
-	p6_msg "$@" >> /tmp/p6/debug.log
+	if p6_debugging_system_on "$systems" "$system"; then
+	    p6_msg "$msg" >> /tmp/p6/debug.log
+	fi
     fi
 
     p6_return_void
@@ -64,7 +70,7 @@ p6_debug() {
 ######################################################################
 p6_log() {
 
-    p6_msg "$@" >> /tmp/p6/log.log
+    p6_msg "$*" >> /tmp/p6/log.log
 
     p6_return_void
 }
@@ -86,6 +92,23 @@ p6_debugging() {
     local rv=$?
 
     p6_return_bool "$rv"
+}
+
+p6_debugging_system_on() {
+    local systems="$1"
+    local system="$2"
+
+    local rc=$P6_FALSE
+
+    case $systems in
+	*$system*) rc=$P6_TRUE ;;
+    esac
+
+    case $systems in
+	*all*) rc=$P6_TRUE ;;
+    esac
+
+    p6_return_code_as_code "$rc"
 }
 
 ######################################################################

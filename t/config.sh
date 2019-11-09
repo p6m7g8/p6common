3aa209626861d0348febb3d9419b89c78fada1c2
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-08 22:46:28 -0500

    tests

--- t/config.sh
+++ t/config.sh
@@ -8,12 +8,20 @@ main() {
     . ../p6test/lib/_bootstrap.sh
     p6_p6test_bootstrap "../p6test"
 
-    p6_test_setup "3"
+    p6_test_setup "7"
 
-    p6_test_start "p6_colors"
+    p6_test_start "p6_config_load"
     (
-	p6_test_run "false"
-	p6_test_assert_run_ok "none"
+	p6_test_run "p6_config_load"
+	p6_test_assert_run_ok "runs" ""
+    )
+    p6_test_finish
+
+    p6_test_start "p6_config_load with file"
+    (
+	P6_PREFIX=$P6_TEST_DIR_ROOT/ p6_test_run "p6_config_load fixtures/defaults.conf"
+	p6_test_assert_run_ok "runs" ""
+	p6_test_assert_eq "$_p6_test_a" "9" "val from file is visible: _p6_test_a is 9"
     )
     p6_test_finish
 

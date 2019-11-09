3c35a8ff6cbac988f074c74d113b7321d92899fc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-09 14:54:25 -0500

    pound out some tests that need work

--- t/dir.sh
+++ t/dir.sh
@@ -8,7 +8,7 @@ main() {
     . ../p6test/lib/_bootstrap.sh
     p6_p6test_bootstrap "../p6test"
 
-    p6_test_setup "210"
+    p6_test_setup "273"
 
     p6_test_start "p6_dir_exists"
     (
@@ -37,55 +37,293 @@ main() {
 
     p6_test_start "p6_dir_load"
     (
-	true
+	p6_test_run "p6_dir_load"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_load" ""
+	p6_test_assert_run_ok "\"\""
+
+	p6_test_run "p6_dir_load" "dne"
+	p6_test_assert_run_ok "dne"
+
+	p6_test_run "p6_dir_load" "exists"
+	p6_test_assert_run_ok "exists but not dir"
+
+	p6_test_run "p6_dir_load" "dir"
+	p6_test_assert_run_ok "exists but dir is empty"
+
+	p6_test_run "p6_dir_load" "dir-1"
+	p6_test_assert_run_ok "dir exists with 1 file"
+
+	p6_test_run "p6_dir_load" "dir-2"
+	p6_test_assert_run_ok "dir exists with 2 files"
+
+	p6_test_run "p6_dir_load" "dir-sd"
+	p6_test_assert_run_ok "dir exists with 1 sub dir"
+
+	p6_test_run "p6_dir_load" "dir-sd-2"
+	p6_test_assert_run_ok "dir exists with 1 sub dir and 2 files"
+
+	p6_test_run "p6_dir_load" "dir-sd-2"
+	p6_test_assert_run_ok "dir exists with 1 sub dir and 2 files"
+
+	p6_test_run "p6_dir_load" "dir-sdw2-2"
+	p6_test_assert_run_ok "dir exists with 1 sub dir with 2 files and 2 files"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_list"
     (
-	true
+	p6_test_run "p6_dir_list"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_list" ""
+	p6_test_assert_run_ok "\"\""
+
+	p6_test_run "p6_dir_list" "dne"
+	p6_test_assert_run_ok "dne"
+
+	p6_test_run "p6_dir_list" "exists"
+	p6_test_assert_run_ok "exists but not dir"
+
+	p6_test_run "p6_dir_list" "dir"
+	p6_test_assert_run_ok "exists but dir is empty"
+
+	p6_test_run "p6_dir_list" "dir-1"
+	p6_test_assert_run_ok "dir exists with 1 file"
+
+	p6_test_run "p6_dir_list" "dir-2"
+	p6_test_assert_run_ok "dir exists with 2 files"
+
+	p6_test_run "p6_dir_list" "dir-sd"
+	p6_test_assert_run_ok "dir exists with 1 sub dir"
+
+	p6_test_run "p6_dir_list" "dir-sd-2"
+	p6_test_assert_run_ok "dir exists with 1 sub dir and 2 files"
+
+	p6_test_run "p6_dir_list" "dir-sd-2"
+	p6_test_assert_run_ok "dir exists with 1 sub dir and 2 files"
+
+	p6_test_run "p6_dir_list" "dir-sdw2-2"
+	p6_test_assert_run_ok "dir exists with 1 sub dir with 2 files and 2 files"
+
+	p6_test_run "p6_dir_list" "dir1 dir2"
+	p6_test_assert_run_ok "dir1 dir2"
     )
     p6_test_finish
 
     p6_test_start "p6_dirs_list"
     (
-	true
+	p6_test_run "p6_dirs_list"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dirs_list" ""
+	p6_test_assert_run_ok "\"\""
+
+	p6_test_run "p6_dirs_list" "dne"
+	p6_test_assert_run_ok "dne"
+
+	p6_test_run "p6_dirs_list" "exists"
+	p6_test_assert_run_ok "exists but not dirs"
+
+	p6_test_run "p6_dirs_list" "dirs"
+	p6_test_assert_run_ok "exists but dirs is empty"
+
+	p6_test_run "p6_dirs_list" "dirs-1"
+	p6_test_assert_run_ok "dirs exists with 1 file"
+
+	p6_test_run "p6_dirs_list" "dirs-2"
+	p6_test_assert_run_ok "dirs exists with 2 files"
+
+	p6_test_run "p6_dirs_list" "dirs-sd"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs"
+
+	p6_test_run "p6_dirs_list" "dirs-sd-2"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs and 2 files"
+
+	p6_test_run "p6_dirs_list" "dirs-sd-2"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs and 2 files"
+
+	p6_test_run "p6_dirs_list" "dirs-sdw2-2"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs with 2 files and 2 files"
+
+	p6_test_run "p6_dirs_list" "dirs1 dirs2"
+	p6_test_assert_run_ok "dirs1 dirs2"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_list_recursive"
     (
-	true
+	p6_test_run "p6_dir_list_recursive"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_list_recursive" ""
+	p6_test_assert_run_ok "\"\""
+
+	p6_test_run "p6_dir_list_recursive" "dne"
+	p6_test_assert_run_ok "dne"
+
+	p6_test_run "p6_dir_list_recursive" "exists"
+	p6_test_assert_run_ok "exists but not dirs"
+
+	p6_test_run "p6_dir_list_recursive" "dirs"
+	p6_test_assert_run_ok "exists but dirs is empty"
+
+	p6_test_run "p6_dir_list_recursive" "dirs-1"
+	p6_test_assert_run_ok "dirs exists with 1 file"
+
+	p6_test_run "p6_dir_list_recursive" "dirs-2"
+	p6_test_assert_run_ok "dirs exists with 2 files"
+
+	p6_test_run "p6_dir_list_recursive" "dirs-sd"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs"
+
+	p6_test_run "p6_dir_list_recursive" "dirs-sd-2"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs and 2 files"
+
+	p6_test_run "p6_dir_list_recursive" "dirs-sd-2"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs and 2 files"
+
+	p6_test_run "p6_dir_list_recursive" "dirs-sdw2-2"
+	p6_test_assert_run_ok "dirs exists with 1 sub dirs with 2 files and 2 files"
+
+	p6_test_run "p6_dir_list_recursive" "dirs1 dirs2"
+	p6_test_assert_run_ok "dirs1 dirs2"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_rmrf"
     (
-	true
+	p6_test_run "p6_dir_rmrf"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_rmrf" ""
+	p6_test_assert_run_ok "\"\""
+
+	p6_test_run "p6_dir_rmrf" "dne"
+	p6_test_assert_run_ok "dne"
+
+	p6_test_run "p6_dir_rmrf" "dir"
+	p6_test_assert_run_ok "dir"
+
+	p6_test_run "p6_dir_rmrf" "file"
+	p6_test_assert_run_ok "file"
+
+	p6_test_run "p6_dir_rmrf" "link"
+	p6_test_assert_run_ok "link"
+
+	p6_test_skip "not bold enough for rm -rf /"
+#	p6_test_run "p6_dir_rmrf" "/"
+#	p6_test_assert_run_ok "/"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_mk"
     (
-	true
+	p6_test_run "p6_dir_mk"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_mk" ""
+	p6_test_assert_run_ok "\"\""
+
+	p6_test_run "p6_dir_mk" "exists"
+	p6_test_assert_run_ok "exists"
+
+	p6_test_run "p6_dir_mk" "new"
+	p6_test_assert_run_ok "new"
+
+	p6_test_run "p6_dir_mk" "1/2"
+	p6_test_assert_run_ok "1/2"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_cp"
     (
-	true
+	p6_test_run "p6_dir_cp"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_cp" "one"
+	p6_test_assert_run_ok "one arg"
+
+	p6_test_run "p6_dir_cp" "one two"
+	p6_test_assert_run_ok "two args but dne"
+
+	p6_test_run "p6_dir_cp" "one two"
+	p6_test_assert_run_ok "two args but dne one"
+
+	p6_test_run "p6_dir_cp" "one two"
+	p6_test_assert_run_ok "two args and dne two"
+
+	p6_test_run "p6_dir_cp" "one two"
+	p6_test_assert_run_ok "two args but exists two"
+
+	p6_test_run "p6_dir_cp" "one two"
+	p6_test_assert_run_ok "two args dne one but exists two"
+
+	p6_test_run "p6_dir_cp" "file dir"
+	p6_test_assert_run_ok "two args file dir"
+
+	p6_test_run "p6_dir_cp" "dir file"
+	p6_test_assert_run_ok "two args dir file"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_mv"
     (
-	true
+	p6_test_run "p6_dir_mv"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_mv" "one"
+	p6_test_assert_run_ok "one arg"
+
+	p6_test_run "p6_dir_mv" "one two"
+	p6_test_assert_run_ok "two args but dne"
+
+	p6_test_run "p6_dir_mv" "one two"
+	p6_test_assert_run_ok "two args but dne one"
+
+	p6_test_run "p6_dir_mv" "one two"
+	p6_test_assert_run_ok "two args and dne two"
+
+	p6_test_run "p6_dir_mv" "one two"
+	p6_test_assert_run_ok "two args but exists two"
+
+	p6_test_run "p6_dir_mv" "one two"
+	p6_test_assert_run_ok "two args dne one but exists two"
+
+	p6_test_run "p6_dir_mv" "file dir"
+	p6_test_assert_run_ok "two args file dir"
+
+	p6_test_run "p6_dir_mv" "dir file"
+	p6_test_assert_run_ok "two args dir file"
     )
     p6_test_finish
 
     p6_test_start "p6_dir_cd"
     (
-	true
+	p6_test_run "p6_dir_cd"
+	p6_test_assert_run_ok "no arg"
+
+	p6_test_run "p6_dir_cd" "one"
+	p6_test_assert_run_ok "one arg but dne"
+
+	p6_test_run "p6_dir_cd" "one two"
+	p6_test_assert_run_ok "two args but dne two"
+
+	p6_test_run "p6_dir_cd" "one"  "two"
+	p6_test_assert_run_ok "two args but dne one"
+
+	p6_test_run "p6_dir_cd" "file"
+	p6_test_assert_run_ok "file"
+
+	p6_test_run "p6_dir_cd" "ilink"
+	p6_test_assert_run_ok "link invalid"
+
+	p6_test_run "p6_dir_cd" "vlink"
+	p6_test_assert_run_ok "link valid"
+
+	p6_test_run "p6_dir_cd" "dir"
+	p6_test_assert_run_ok "dir"
     )
     p6_test_finish
 

488ccfd68902c71ddaf497fcd8a17ac0b84e9ba2
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-08 22:46:58 -0500

    tests

--- t/const.sh
+++ t/const.sh
@@ -8,12 +8,17 @@ main() {
     . ../p6test/lib/_bootstrap.sh
     p6_p6test_bootstrap "../p6test"
 
-    p6_test_setup "3"
+    p6_test_setup "7"
 
-    p6_test_start "p6_colors"
+    p6_test_start "p6_constants"
     (
-	p6_test_run "false"
-	p6_test_assert_run_ok "none"
+	p6_test_assert_eq "$P6_TRUE"  "0" "P6_TRUE is 0"
+	p6_test_assert_eq "$P6_FALSE" "1" "P6_FALSE is 1"
+	p6_test_assert_eq "$P6_RETURN_SUCCESS" "0" "P6_RETURN_SUCCESS is 0"
+	p6_test_assert_eq "$P6_RETURN_FAIL" "1" "P6_RETURN_FAIL is 1"
+	p6_test_assert_eq "$P6_EXIT_SUCCESS" "0" "P6_EXIT_SUCCESS is 0"
+	p6_test_assert_eq "$P6_EXIT_FAIL" "1" "P6_EXIT_FAIL is 1"
+	p6_test_assert_eq "$P6_EXIT_ARGS" "2" "P6_EXIT_ARGS is 2"
     )
     p6_test_finish
 

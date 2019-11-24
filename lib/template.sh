0c98f22ee50ab87b7ad111c86e0178cf1e675aff
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-24 17:38:22 -0500

    feat(template): simplifies parsing by not passing sed_res around

--- lib/template.sh
+++ lib/template.sh
@@ -18,101 +18,37 @@ p6_template__debug() {
 ######################################################################
 #<
 #
-# Function: p6_template_fill_in(infile, outfile, q_flag)
+# Function: str processed = p6_template_process(infile, ...)
 #
 #  Args:
 #	infile - 
-#	outfile - 
-#	q_flag - 
-#
-#>
-######################################################################
-p6_template_fill_in() {
-    local infile="$1"
-    local outfile="$2"
-    local q_flag="$3"
-    shift 3
-
-    p6_file_copy "$infile" "$outfile"
-
-    local sed_re
-    local save_ifs=$IFS
-    IFS=^
-    for sed_re in $(p6_echo $@); do
-	if p6_string_eq "$q_flag" "no_quotes"; then
-	    p6_template__debug "N: sed -i ''  -e $sed_re $outfile"
-	    sed -i '' -e $sed_re $outfile
-	else
-	    p6_template__debug "Q: sed -i ''  -e \"$sed_re\" $outfile"
-	    sed -i '' -e "$sed_re" $outfile
-	fi
-    done
-    IFS=$save_ifs
-
-    p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: str fills = p6_template_fill_args(mark, sep, split)
-#
-#  Args:
-#	mark - 
-#	sep - 
-#	split - 
+#	... - k=v replacements
 #
 #  Returns:
-#	str - fills
-#
-#>
-######################################################################
-p6_template_fill_args() {
-    local mark="$1"
-    local sep="$2"
-    local split="$3"
-    shift 3
-
-    local pair
-    local args
-    local save_ifs=$IFS
-    IFS=$split
-    for pair in $(p6_echo $@); do
-	local k=$(p6_echo "$pair" | cut -f 1 -d '=')
-	local v=$(p6_echo "$pair" | cut -f 2- -d '=')
-
-	args="${args}s${sep}${mark}${k}${mark}${sep}${v}${sep}g^"
-    done
-    IFS=$save_ifs
-
-    local fills=$(p6_echo "$args" | sed -e 's,\^$,,')
-
-    p6_return_str "$fills";
-}
-
-######################################################################
-#<
-#
-# Function: p6_template_process(infile)
-#
-#  Args:
-#	infile - 
+#	str - processed
 #
 #>
 ######################################################################
 p6_template_process() {
     local infile="$1"
-    shift 1
+    shift 1 # k=v replacements
 
-    local dir=$(p6_transient_create "p6.tmpl")
+    local dir=$(p6_transient_create "p6_template")
     local outfile="$dir/outfile"
 
-    local fill_args=$(p6_template_fill_args "" "%" " " "$@")
+    p6_file_copy "$infile" "$outfile"
 
-    p6_template_fill_in "$infile" "$outfile" "" "$fill_args"
-    p6_file_display "$outfile"
+    local pair
+    for pair in "$@"; do
+	local k=$(p6_echo "$pair" | cut -d '=' -f 1)
+	local v=$(p6_echo "$pair" | cut -d '=' -f 2-)
+
+	p6_file_repalce "$outfile" "s/$k/$v/g"
+    done
+
+    local processed=$(p6_file_display "$outfile")
 
     p6_transient_delete "$dir"
 
-    p6_return_void
+    p6_return_str "$processed"
 }

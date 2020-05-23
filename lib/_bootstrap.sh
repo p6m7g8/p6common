abaa8e4dde8abca1c7d532cbc61e6b484ae127f9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-23 19:13:59 -0400

    feat(optimizing): if local, we know we've been bootstraped

--- lib/_bootstrap.sh
+++ lib/_bootstrap.sh
@@ -10,8 +10,9 @@
 ######################################################################
 p6_bootstrap() {
   local dir="${1:-$P6_DFZ_SRC_P6M7G8_DIR/p6common}"
+  local islocal="${2:-}"
 
-  p6_bootstrap_optimize "$dir"
+  p6_bootstrap_optimize "$dir" "$islocal"
 
   local file
   for file in $(p6_dir_list_recursive "$dir/lib"); do
@@ -36,13 +37,16 @@ p6_bootstrap() {
 ######################################################################
 p6_bootstrap_optimize() {
     local dir="$1"
+    local islocal="$2"
 
-    # XXX: Chicken in Egg....
-    . $dir/../p6common/lib/const.sh
-    . $dir/../p6common/lib/return.sh
-    . $dir/../p6common/lib/io.sh
-    . $dir/../p6common/lib/debug.sh
-    . $dir/../p6common/lib/string.sh
-    . $dir/../p6common/lib/file.sh
-    . $dir/../p6common/lib/dir.sh
+    if [ x"$islocal" != x"local" ]; then
+      # XXX: Chicken in Egg....
+      . $dir/../p6common/lib/const.sh
+      . $dir/../p6common/lib/return.sh
+      . $dir/../p6common/lib/io.sh
+      . $dir/../p6common/lib/debug.sh
+      . $dir/../p6common/lib/string.sh
+      . $dir/../p6common/lib/file.sh
+      . $dir/../p6common/lib/dir.sh
+    fi
 }

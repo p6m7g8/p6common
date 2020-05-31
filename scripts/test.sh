8c4072b0151c5becaeeb68433aa9d89f6277a693
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-31 13:38:50 -0400

    feat(cicd): adds doc and lint (#14)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-. ../p6common/lib/_bootstrap.sh
+. ${ROOT_DIR_REL:-..}/p6common/lib/_bootstrap.sh
 p6_bootstrap
 
-p6_bootstrap ../p6test
+p6_bootstrap ${ROOT_DIR_REL:-..}/p6test
 
 p6_p6test_bootstrap
 

5ed83669b4bf9ce481f2f63326b5b955b31401aa
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-17 16:40:44 -0400

    fix(doc): restore doc gens in script/doc.sh

---
+++ scripts/doc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+. ${ROOT_DIR_REL:-..}/p6common/lib/_bootstrap.sh
+p6_bootstrap
+
+perl ${ROOT_DIR_REL:-}/p6perl/bin/doc_inline.pl --module .
+perl ${ROOT_DIR_REL:-}/p6perl/bin/doc_readme.pl --module . >README.md

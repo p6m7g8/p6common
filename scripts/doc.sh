29b1a79dd38ced655f3bf16916bdc5c8e6a6661f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-10-11 18:06:20 -0400

    fix(doc): do not need p6common here

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-. ${ROOT_DIR_REL:-..}/p6common/lib/_bootstrap.sh
-p6_bootstrap
-
 perl ${ROOT_DIR_REL:-}/p6perl/bin/doc_inline.pl --module .
 perl ${ROOT_DIR_REL:-}/p6perl/bin/doc_readme.pl --module . >README.md

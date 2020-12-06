14658b16484c8cb2a276591a46de8d5d2c712e52
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-06 17:29:08 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-pwd
-ls ..
+echo "========> CPANM"
+curl -L https://cpanmin.us | perl App::cpanminus
 
-cpan install Data::Dumper
+echo "=========> Data::Dumper"
+cpanm --notests --force Data::Dumper
+
+echo "=========> p6perl"
 if [ ! -d ../p6perl ]; then
   git clone https://github.com/p6m7g8/p6perl ../p6perl
 fi

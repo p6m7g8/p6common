94a3b000151b56fc47651e6aac25f753bdf52909
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-07 10:36:24 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -9,3 +9,6 @@ echo "=========> p6perl"
 if [ ! -d ../p6perl ]; then
     git clone https://github.com/p6m7g8/p6perl ../p6perl
 fi
+if [ ! -d ../p6test ]; then
+    git clone https://github.com/p6m7g8/p6test ../p6test
+fi

662c393532932491241d62e6e9ee1e8512d1eb7f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-06 20:30:18 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
 
-set +e
-uname -a
-cat /etc/*release
-cat /etc/issue
-echo -----------------
-df -h
-echo -----------------
-pwd
-echo -----------------
-ls -alF ..
-echo -----------------
-ls -alF
-echo -----------------
-env | sort
-echo -----------------
-echo $PATH
-echo -----------------
-yum --version
-apk --version
-apt-get --version
-apt --version
-git --version
+apk --no-cache add git
+apk --no-cache add bash
+apk --no-cache add perl
 
-apk --no-cache list installed
+cpan install Data::Dumper

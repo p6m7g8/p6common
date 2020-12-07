3b06dbd0c73df5a7a52805bf5ac2265237037b03
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-07 10:16:58 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,6 +3,7 @@
 apk --no-cache add bash
 apk --no-cache add git
 apk --no-cache add wget
+apk --no-cache add clang
 apk --no-cache add make
 apk --no-cache add perl
 apk --no-cache add perl-app-cpanminus

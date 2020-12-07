868b1853c00d29b330ea4d234ba34f5c8fe88451
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-07 10:43:48 -0500

    fixes

--- t/alias.sh
+++ t/alias.sh
@@ -2,35 +2,35 @@
 
 main() {
 
-    . ../p6common/lib/_bootstrap.sh
-    p6_bootstrap "../p6common"
+	. ../p6common/lib/_bootstrap.sh
+	p6_bootstrap "../p6common"
 
-    . ../p6test/lib/_bootstrap.sh
-    p6_p6test_bootstrap "../p6test"
+	. ../p6test/lib/_bootstrap.sh
+	p6_p6test_bootstrap "../p6test"
 
-    p6_test_setup "12"
+	p6_test_setup "12"
 
-    p6_test_start "p6_alias"
-    (
-	p6_test_run "p6_alias"
-	p6_test_assert_run_ok "no arg"
+	p6_test_start "p6_alias"
+	(
+		p6_test_run "p6_alias"
+		p6_test_assert_run_ok "no arg"
 
-	p6_test_run "p6_alias" "A"
-	p6_test_assert_run_ok "one arg"
+		p6_test_run "p6_alias" "A"
+		p6_test_assert_run_ok "one arg"
 
-	p6_test_run "p6_alias" "C" "'echo 777'"
-	p6_test_assert_run_ok "two args"
+		p6_test_run "p6_alias" "C" "'echo 777'"
+		p6_test_assert_run_ok "two args"
 
-	p6_test_run "alias B='echo 777'; B"
-	p6_test_assert_eq "$(p6_test_run_stdout)" "777" "alias works"
-	p6_test_assert_blank "$(p6_test_run_stderr)" "no stderr"
+		p6_test_run "alias B='echo 777'; B"
+		p6_test_assert_eq "$(p6_test_run_stdout)" "777" "alias works"
+		p6_test_assert_blank "$(p6_test_run_stderr)" "no stderr"
 
-	p6_test_run "p6_alias" "B"
-	p6_test_todo "$(p6_test_run_stdout)" "'echo 777'" "find alias" "not impl"
-    )
-    p6_test_finish
+		p6_test_run "p6_alias" "B"
+		p6_test_todo "$(p6_test_run_stdout)" "'echo 777'" "find alias" "not impl"
+	)
+	p6_test_finish
 
-    p6_test_teardown
+	p6_test_teardown
 }
 
 main "$@"

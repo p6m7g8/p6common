61c72b464073b3794f32adedf8595c87ad75fc87
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-07 10:20:52 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-apk --no-cache add bash
-apk --no-cache add git
-apk --no-cache add wget
-apk --no-cache add gcc
-apk --no-cache add make
-apk --no-cache add perl
-apk --no-cache add perl-app-cpanminus
-
-cpanm -v --notest --force ExtUtils::Manifest
-cpanm -v --notest --force Data::Dumper
+echo true

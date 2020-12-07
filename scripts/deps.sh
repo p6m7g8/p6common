43db0a32d789070d44fb39814d319b9f35610031
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-06 19:35:00 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
+set +e
+uname -a
+cat /etc/*release
+cat /etc/issue
+echo -----------------
+df -h
+echo -----------------
+pwd
+echo -----------------
+ls -alF ..
+echo -----------------
+ls -alF
+echo -----------------
+env | sort
+echo -----------------
+echo $PATH
+echo -----------------
+yum --version
+apk --version
+apt-get --version
+apt --version
+
 echo "========> CPANM"
 curl -L https://cpanmin.us/ -o cpanm
 chmod +x cpanm
 
 echo "=========> Data::Dumper"
-./cpanm --notest --force ExtUtils::Manifest Data::Dumper
+./cpanm -v --notest --force ExtUtils::Manifest Data::Dumper
 
 echo "=========> p6perl"
 if [ ! -d ../p6perl ]; then

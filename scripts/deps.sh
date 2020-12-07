bbeee45808fd29cb6219e315f057eba1af33be1b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-07 10:25:04 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo true
+echo "=========> p5-Data::Dumper"
+cpanm -v --notest --force Data::Dumper
+
+echo "=========> p6perl"
+if [ ! -d ../p6perl ]; then
+    git clone https://github.com/p6m7g8/p6perl ../p6perl
+fi

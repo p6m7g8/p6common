ee10b21f67ba54b915c496e8c2fd68bb019b3369
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-07 14:14:26 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -8,9 +8,6 @@ fi
 echo "=========> cpanm"
 yum install perl-CPAN -y
 
-echo "=========> p5-Data::Dumper"
-cpanm -v --notest --force Data::Dumper
-
 echo "=========> p6perl"
 if [ ! -d ../p6perl ]; then
     git clone https://github.com/p6m7g8/p6perl ../p6perl

8148da34ddb4ae7d9d2221dffced741baef8fb78
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-06 20:40:04 -0500

    fixes

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -7,4 +7,15 @@ apk --no-cache add git
 apk --no-cache add bash
 apk --no-cache add perl
 
-/usr/bin/cpan install Data::Dumper
+echo ----------------------
+ls /bin
+echo ----------------------
+ls /sbin
+echo ----------------------
+ls /usr/bin
+echo ----------------------
+ls /usr/sbin
+echo ----------------------
+ls /usr/local/bin
+
+#/usr/bin/cpan install Data::Dumper

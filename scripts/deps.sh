f5b51eb77951c6c3997c7d3871835b8ede62778b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-04 12:55:05 -0400

    chore: testing release

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -2,9 +2,6 @@
 
 apk --no-cache add git ncurses
 
-echo "=========> p5-Data::Dumper"
-cpanm -v --notest --force Data::Dumper
-
 echo "=========> p6perl"
 if [ ! -d ../p6perl ]; then
     git clone https://github.com/p6m7g8/p6perl ../p6perl

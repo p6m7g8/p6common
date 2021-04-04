8f417674751b817cf1b1a71832ddc20b34ecb757
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-03 20:05:53 -0400

    feat(api): adds network.sh (#124)

--- init.zsh
+++ init.zsh
@@ -4,6 +4,8 @@
 #
 # Function: p6df::modules::p6common::init()
 #
+#  Depends:	 p6_bootstrap
+#  Environment:	 P6_DFZ_SRC_DIR
 #>
 ######################################################################
 p6df::modules::p6common::init() {

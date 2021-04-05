7a5dd7372502b78995d2286df19ae64bd2ba6da7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-05 14:32:32 -0400

    chore(cicd): drops Makefile

--- lib/cicd/build.sh
+++ lib/cicd/build.sh
@@ -8,6 +8,7 @@
 #>
 ######################################################################
 p6_cicd_build_run() {
+  local dockerfile="${1:-Dockerfile}"
 
-  docker build .
+  docker -l debug build --no-cache --progress plain -f "$dockerfile" .
 }

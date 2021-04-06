b1bafff2cf295a11a609f4441c7ffc555196ad3b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-05 23:00:21 -0400

    fixes

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,8 @@ WORKDIR /p6m7g8/p6common
 COPY . .
 
 RUN apk --no-cache add git
+
+ENV DOCKER_WORKFLOW Build
 RUN bin/deps.sh
 
 RUN ../p6ctl/bin/p6ctl docker_build
--- bin/deps.sh
+++ bin/deps.sh
@@ -1,9 +1,13 @@
-#!/bin/bash -x
+#!/bin/sh -x
 
-cd /p6m7g8/p6common
-
-if [ ! -d ../p6ctl ]; then
-  git clone https://github.com/p6m7g8/p6ctl ../p6ctl
+if [ -n "$GITHUB_WORKFLOW" ]; then
+    apk --no-cache add git    
+    git clone https://github.com/p6m7g8/p6ctl ../p6ctl
+elif [ -n "$DOCKER_WORKFLOW" ]; then
+    apk --no-cache add git    
+    (cd /p6m7g8/p6common ; git clone https://github.com/p6m7g8/p6ctl ../p6ctl)
+else
+    # laptop in PATH already
+    true
 fi
-
 

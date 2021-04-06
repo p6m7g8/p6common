616fb2766845485ff7a78cd1d3de4433518c8a7e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-05 23:08:44 -0400

    chore: take 2 (#143)

--- bin/deps.sh
+++ bin/deps.sh
@@ -5,7 +5,7 @@ if [ -n "$GITHUB_WORKFLOW" ]; then
     git clone https://github.com/p6m7g8/p6ctl ../p6ctl
 elif [ -n "$DOCKER_WORKFLOW" ]; then
     apk --no-cache add git    
-    (cd /p6m7g8/p6common ; git clone https://github.com/p6m7g8/p6ctl ../p6ctl)
+    git clone https://github.com/p6m7g8/p6ctl /p6m7g8/p6ctl
 else
     # laptop in PATH already
     true
